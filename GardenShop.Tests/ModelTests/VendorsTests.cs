d63aaedd484f65d5a2d41ce00c552b40c5cc9f2f
jessicahattig <jessicahattig@gmail.com>
2023-09-20 19:29:17 -0700

    BUGFIX: make ModelTests files plural.

---
+++ GardenShop.Tests/ModelTests/VendorsTests.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GardenShop.Models;
+using System.Collections.Generic;
+using System;
+
+namespace GardenShop.TestTools
+{
+  [TestClass]
+  public class Vendors
+}
